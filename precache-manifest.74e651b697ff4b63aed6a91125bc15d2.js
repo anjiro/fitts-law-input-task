3c5db115d92ab5dc7ef7c0261753421ace24cb6c
Daniel Ashbrook <dan@danielashbrook.com>
2022-05-02 14:20:05 +0200

    Deploy React app to GitHub Pages

---
+++ precache-manifest.74e651b697ff4b63aed6a91125bc15d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ed7a37f021de4a74b2224d607a18266",
+    "url": "/fitts-law-input-task/index.html"
+  },
+  {
+    "revision": "756a7f738f813dc93d79",
+    "url": "/fitts-law-input-task/static/css/main.49152521.chunk.css"
+  },
+  {
+    "revision": "7ff0b615505482f4f314",
+    "url": "/fitts-law-input-task/static/js/2.59ff889c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/fitts-law-input-task/static/js/2.59ff889c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "756a7f738f813dc93d79",
+    "url": "/fitts-law-input-task/static/js/main.af6824f0.chunk.js"
+  },
+  {
+    "revision": "e750ab85f3d424cd270b",
+    "url": "/fitts-law-input-task/static/js/runtime-main.f964db4e.js"
+  }
+]);
